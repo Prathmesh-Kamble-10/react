16dd20da8bfa4151c351fade4805df31d7bf61b4
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-08-19 02:25:36 +0530

    currencyChanger app is successfully build

---
+++ currency_changer/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+  <>
+  <h1 className='text-3xl text-white'>Currency Changer</h1>
+  </>
+  )
+}
+
+export default App
